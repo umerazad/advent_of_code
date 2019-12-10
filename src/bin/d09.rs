6e5e6547a69c7d8df144b4845c82ec5d70f37c7a
Muhammad Umer Azad <umer@fastmail.com>
2019-12-09 16:34:35 -0800

    Day 9 - Part 2.

--- src/bin/d09.rs
+++ src/bin/d09.rs
@@ -1,10 +1,31 @@
 use aoc2019::read_csv_ints;
 use aoc2019::vm::VM;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_day9_part1() {
+        let program = read_csv_ints("assets/day9_input");
+        let mut vm = VM::new(program);
+        vm.set_inputs(&[1]);
+        vm.run();
+        assert_eq!(vm.get_last_output(), 3598076521);
+    }
+
+    fn test_day9_part2() {
+        let program = read_csv_ints("assets/day9_input");
+        let mut vm = VM::new(program);
+        vm.set_inputs(&[1]);
+        vm.run();
+        assert_eq!(vm.get_last_output(), 90722);
+    }
+}
+
 fn main() {
     let program = read_csv_ints("assets/day9_input");
     let mut vm = VM::new(program);
-    vm.set_inputs(&[1]);
+    vm.set_inputs(&[2]);
     vm.run();
     println!("Outputs: {:?}", vm.outputs());
 }
